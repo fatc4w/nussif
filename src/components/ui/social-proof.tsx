465b543abd8c99f4918ac126c2ca2f300ab5476d
kzfsg <131367782+kzfsg@users.noreply.github.com>
2026-03-18 16:02:03 +0800

    Update social-proof.tsx
    fixed bug when stacking wasnt proper

--- src/components/ui/social-proof.tsx
+++ src/components/ui/social-proof.tsx
@@ -22,7 +22,7 @@ export default function SocialProof({ partners, title }: SocialProofProps) {
           </h2>
         )}
 
-        <div className="grid grid-cols-1 border border-border md:grid-cols-3 lg:grid-cols-5">
+        <div className="grid grid-cols-1 border border-border sm:grid-cols-5">
           {partners.map((partner, index) => (
             <motion.div
               key={partner.name}
@@ -30,7 +30,7 @@ export default function SocialProof({ partners, title }: SocialProofProps) {
               whileInView={{ opacity: 1 }}
               viewport={{ once: true }}
               transition={{ duration: 0.4, delay: index * 0.05 }}
-              className="flex items-center justify-center border-b border-border bg-background px-8 py-8 last:border-b-0 md:border-r md:[&:nth-child(3n)]:border-r-0 md:[&:nth-last-child(-n+3)]:border-b-0 lg:[&:nth-child(3n)]:border-r lg:[&:nth-child(5n)]:border-r-0 lg:[&:nth-last-child(-n+5)]:border-b-0"
+              className="flex items-center justify-center border-b border-border bg-background px-8 py-8 last:border-b-0 sm:border-r sm:last:border-r-0 sm:border-b-0"
             >
               <img
                 src={partner.logo}
